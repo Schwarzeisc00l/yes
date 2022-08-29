77f21f4c37259f4ab9e606a912c76da52ee0d267
Schwarzeisc00l <78359804+Schwarzeisc00l@users.noreply.github.com>
2022-08-29 20:58:29 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-wget 'https://download.microsoft.com/download/0/6/3/06365375-C346-4D65-87C7-EE41F55F736B/7601.24214.180801-1700.win7sp1_ldr_escrow_CLIENT_PROFESSIONAL_x64FRE_en-us.iso'
-mv '7601.24214.180801-1700.win7sp1_ldr_escrow_CLIENT_PROFESSIONAL_x64FRE_en-us.iso' 'windows7.iso'
-sudo dd if=windows7.iso of=/dev/sdb bs=1M
+wget 'https://ftp.linux.org.tr/linuxmint/iso/stable/21/linuxmint-21-xfce-64bit.iso'
+mv 'linuxmint-21-xfce-64bit.iso' 'linux.iso'
+sudo dd if=linux.iso of=/dev/sdb bs=1M
 echo 'Eger icinde error vs. iceren bi yazi gelmediyse islem tamamlandi.'
 
